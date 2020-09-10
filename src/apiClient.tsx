e1f51900ee14d6d3799b8d29611dd5c234f6fecb
mateoochandorena <mateochando@gmail.com>
2020-09-10 09:06:51 +0100

    server.js file

--- src/apiClient.tsx
+++ src/apiClient.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 const apiClient = {
     getAllRestaurants(location: {lat:number, long: number}, radius: number){
-        return axios.get('https://restaurants-server.herokuapp.com//get-restaurants', { params: {latitude: location.lat, longitude: location.long, radius: radius}, data: {location: location, radius: radius}});
+        return axios.get('https://restaurants-server.herokuapp.com/get-restaurants', { params: {latitude: location.lat, longitude: location.long, radius: radius}, data: {location: location, radius: radius}});
     },
     getRestaurantDetail(id: string){
-        return axios.get('https://restaurants-server.herokuapp.com//get-restaurant-detail', {params:{ id }})
+        return axios.get('https://restaurants-server.herokuapp.com/get-restaurant-detail', {params:{ id }})
     }
 }
 
