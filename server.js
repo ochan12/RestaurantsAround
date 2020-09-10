9670f983df0a81a784bb566711c91f00d4bc30ad
mateoochandorena <mateochando@gmail.com>
2020-09-10 09:26:50 +0100

    server.js file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
-
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'build')))
 
+const cors = require('cors')
+app.use(cors())
 var port = process.env.PORT || 8000
 app.listen(port)
 console.log('server started ' + port)
